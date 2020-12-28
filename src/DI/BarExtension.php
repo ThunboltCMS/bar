f7d30398570adefe21b55ebbac43d38ee9fb04fa
MartkCz <martk494@gmail.com>
2020-12-28 13:38:42 +0100

    refactoring

--- src/DI/BarExtension.php
+++ src/DI/BarExtension.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace Thunbolt\Bar\DI;
 
-use Nette;
+use Doctrine\ORM\EntityManagerInterface;
 use Nette\DI\CompilerExtension;
 use Doctrine\ORM\EntityManager;
+use Nette\PhpGenerator\ClassType;
+use Nette\Schema\Expect;
+use Nette\Schema\Schema;
 use Thunbolt\Bar\BarException;
 use Thunbolt\Bar\Bars\DoctrineBar;
 use Thunbolt\Bar\Bars\LogBar;
@@ -34,66 +37,78 @@ class BarExtension extends CompilerExtension {
 	/** @var string */
 	private $tracyBarService;
 
+	public function getConfigSchema(): Schema
+	{
+		$builder = $this->getContainerBuilder();
+
+		return Expect::structure([
+			'temp' => Expect::structure([
+				'enable' => Expect::bool(true),
+				'tempDir' => Expect::string($builder->parameters['tempDir']),
+			]),
+			'doctrine' => Expect::structure([
+				'enable' => Expect::bool(class_exists(EntityManagerInterface::class)),
+			]),
+			'log' => Expect::structure([
+				'enable' => Expect::bool(Debugger::$logDirectory && is_dir(Debugger::$logDirectory)),
+				'logDir' => Expect::string(Debugger::$logDirectory),
+			]),
+		]);
+	}
+
 	/**
 	 * Processes configuration data. Intended to be overridden by descendant.
 	 */
 	public function loadConfiguration(): void {
-		$this->processDefaults();
 		$builder = $this->getContainerBuilder();
-		$config = $this->validateConfig($this->defaults, $this->getConfig());
+		$config = $this->getConfig();
 
-		$this->tracyBarService = $this->getContainerBuilder()->getByType(Bar::class);
+		$this->tracyBarService = $this->getContainerBuilder()
+			->getByType(Bar::class);
 		if (!$this->tracyBarService) {
 			throw new BarException('Tracy bar not found in container.');
 		}
 
-		if ($config['temp']['enable']) {
-			if (!$config['temp']['tempDir']) {
+		if ($config->temp->enable) {
+			if (!$config->temp->tempDir) {
 				throw new BarException('Temp dir has not been set.');
 			}
 			$builder->addDefinition($this->prefix('temp'))
-				->setFactory(TempBar::class, [$config['temp']['tempDir']]);
+				->setFactory(TempBar::class, [$config->temp->tempDir]);
 		}
-		if ($config['doctrine']['enable']) {
+		if ($config->doctrine->enable) {
 			$builder->addDefinition($this->prefix('doctrine'))
 				->setType(DoctrineBar::class);
 		}
-		if ($config['log']['enable']) {
+		if ($config->log->enable && $config->log->logDir) {
 			$builder->addDefinition($this->prefix('log'))
-				->setFactory(LogBar::class, [$config['log']['logDir']]);
-		}
-	}
-
-	public function processDefaults(): void {
-		$this->defaults['doctrine']['enable'] = class_exists(EntityManager::class);
-		$this->defaults['log']['enable'] = Debugger::$logDirectory && is_dir(Debugger::$logDirectory);
-		$this->defaults['log']['logDir'] = Debugger::$logDirectory;
-		if (isset($this->getContainerBuilder()->parameters['tempDir'])) {
-			$this->defaults['temp']['tempDir'] = $this->getContainerBuilder()->parameters['tempDir'];
+				->setFactory(LogBar::class, [$config->log->logDir]);
 		}
 	}
 
 	/**
 	 * Adjusts DI container compiled to PHP class. Intended to be overridden by descendant.
-	 *
-	 * @param Nette\PhpGenerator\ClassType $class
 	 */
-	public function afterCompile(Nette\PhpGenerator\ClassType $class): void {
+	public function afterCompile(ClassType $class): void {
 		$builder = $this->getContainerBuilder();
 		$init = $class->getMethods()['initialize'];
 
+		if ($builder->parameters['consoleMode']) {
+			return;
+		}
+
 		if ($builder->hasDefinition($this->prefix('temp'))) {
-			$init->addBody('if ($this->parameters["debugMode"]) $this->getService(?)->addPanel($this->getService(?));', [
+			$init->addBody('$this->getService(?)->addPanel($this->getService(?));', [
 				$this->tracyBarService, $this->prefix('temp')
 			]);
 		}
 		if ($builder->hasDefinition($this->prefix('doctrine'))) {
-			$init->addBody('if ($this->parameters["debugMode"]) $this->getService(?)->addPanel($this->getService(?));', [
+			$init->addBody('$this->getService(?)->addPanel($this->getService(?));', [
 				$this->tracyBarService, $this->prefix('doctrine')
 			]);
 		}
 		if ($builder->hasDefinition($this->prefix('log'))) {
-			$init->addBody('if ($this->parameters["debugMode"]) $this->getService(?)->addPanel($this->getService(?));', [
+			$init->addBody('$this->getService(?)->addPanel($this->getService(?));', [
 				$this->tracyBarService, $this->prefix('log')
 			]);
 		}
